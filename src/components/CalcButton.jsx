34c139d6aa39081b7d7e9217d147948855b23fb5
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-13 13:28:15 -0500

    Update CalcButton.jsx

--- src/components/CalcButton.jsx
+++ src/components/CalcButton.jsx
@@ -1,1 +1,16 @@
+import { motion } from "framer-motion";
+import { Button } from "@mui/material";
 
+export default function CalcButton({ value, onClick }) {
+  return (
+    <motion.div whileTap={{ scale: 0.9 }}>
+      <Button
+        variant="contained"
+        onClick={() => onClick(value)}
+        sx={{ fontSize: "1.2rem", p: 2 }}
+      >
+        {value}
+      </Button>
+    </motion.div>
+  );
+}
