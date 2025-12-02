53c63a72418c5c20fb3f0fb5e1eba5d4d12c7cb7
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-02 00:13:47 -0500

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,24 +1,25 @@
 import { Box, Typography } from "@mui/material";
 
 export default function Display({ value, darkMode }) {
-return (
-<Box
-sx={{
-p: 2,
-mb: 2,
-borderRadius: 2,
-bgcolor: darkMode ? "#121212" : "#f0f0f0",
-textAlign: "right",
-minHeight: 70,
-display: "flex",
-alignItems: "center",
-justifyContent: "flex-end",
-overflowX: "auto",
-}}
->
-<Typography variant="h5" sx={{ wordBreak: "break-word" }}>
-{value || "0"}
-</Typography>
-</Box>
-);
+  return (
+    <Box
+      sx={{
+        width: "100%",                // 🔥 hace que coincida con teclado
+        p: 2,
+        mb: 2,
+        borderRadius: 2,
+        bgcolor: darkMode ? "#121212" : "#f0f0f0",
+        textAlign: "right",
+        minHeight: 70,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        overflowX: "auto",
+      }}
+    >
+      <Typography variant="h5" sx={{ wordBreak: "break-word" }}>
+        {value || "0"}
+      </Typography>
+    </Box>
+  );
 }
