00f422290a2b3403a1a490e47e255602800f8a02
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-02 00:24:24 -0500

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,32 +1,24 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Display({ value, darkMode }) {
-  return (
-    <Grid container spacing={1}>
-      <Grid item xs={12}>
-        <Box
-          sx={{
-            width: "100%",
-            p: 2,
-            borderRadius: 2,
-            bgcolor: darkMode ? "#121212" : "#f0f0f0",
-            textAlign: "right",
-            minHeight: 70,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            overflowX: "auto",
-            boxSizing: "border-box",
-          }}
-        >
-          <Typography
-            variant="h5"
-            sx={{ wordBreak: "break-word", width: "100%" }}
-          >
-            {value || "0"}
-          </Typography>
-        </Box>
-      </Grid>
-    </Grid>
-  );
+return (
+<Box
+sx={{
+p: 2,
+mb: 2,
+borderRadius: 2,
+bgcolor: darkMode ? "#121212" : "#f0f0f0",
+textAlign: "right",
+minHeight: 70,
+display: "flex",
+alignItems: "center",
+justifyContent: "flex-end",
+overflowX: "auto",
+}}
+>
+<Typography variant="h5" sx={{ wordBreak: "break-word" }}>
+{value || "0"}
+</Typography>
+</Box>
+);
 }
